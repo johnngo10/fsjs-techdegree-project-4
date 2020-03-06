aa7baa83edb459d8c3c5d0ab86bf20f9f3a0a06d
xuanngo10 <xuan.ngo10@gmail.com>
2020-03-05 21:42:49 -0800

    progress

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,40 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor(missed, phrases, activePhrase) {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = null;
+  }
+
+  /**
+   * Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases() {
+    const fivePhrases = [
+      { phrase: "I like tacos" },
+      { phrase: "Javascript is fun" },
+      { phrase: "I enjoy coding" },
+      { phrase: "I want to travel" },
+      { phrase: "full stack developer" }
+    ];
+
+    return fivePhrases;
+  }
+
+  /**
+   * Selects random phrase from phrases property
+   * @return {Object} Phrase object chosen to be used
+   */
+  getRandomPhrase() {
+    const phraseLength = this.phrases.length;
+    const randomNum = Math.floor(Math.random() * (phraseLength - 0)) + 0;
+
+    return this.phrases[randomNum];
+  }
+
+  startGame() {}
+}
