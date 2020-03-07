946b3d066e8baa1f74fa4fc17cfe9e7794673cbd
xuanngo10 <xuan.ngo10@gmail.com>
2020-03-06 22:10:32 -0800

    added display phrase method

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,13 +4,27 @@
 
 class Phrase {
   constructor(phrase) {
-    // this.phrase = phrase.toLowerCase();
+    this.phrase = phrase;
   }
 
   /**
    * Display phrase on game board
    */
-  addPhraseToDisplay() {}
+  addPhraseToDisplay() {
+    const phraseDisplay = document.getElementById("phrase");
+    const phraseList = phraseDisplay.firstElementChild;
+    const thePhrase = this.phrase;
+    console.log(thePhrase);
+    for (let i = 0; i < thePhrase.length; i++) {
+      if (thePhrase[i] !== " ") {
+        console.log("letter");
+        phraseList.innerHTML += `<li class="hide letter ${thePhrase[i]}">${thePhrase[i]}</li>`;
+      } else if (thePhrase[i] === " ") {
+        console.log("space");
+        phraseList.innerHTML += `<li class="space"></li>`;
+      }
+    }
+  }
 
   checkLetter() {}
 
--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const phrase = new Phrase();
 const game = new Game();
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase(randomPhrase.phrase);
+phrase.addPhraseToDisplay();
