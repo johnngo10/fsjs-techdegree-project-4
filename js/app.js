e04f12a6d6326ba172d8371af83ad6d3eed02bcc
xuanngo10 <xuan.ngo10@gmail.com>
2020-03-12 18:23:57 -0700

    added fix

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const game = new Game();
+let game = "";
 const phrase = new Phrase();
 
 // start game or reset game
 document.getElementById("btn__reset").addEventListener("click", () => {
+  game = new Game();
   game.resetGame();
   game.startGame();
 });
@@ -16,13 +17,13 @@ const keyboard = document.getElementById("qwerty");
 // disable clicked buttons
 keyboard.addEventListener("click", btn => {
   const button = btn.target;
-
   if (button.className === "key") {
     game.handleInteraction(button);
     button.setAttribute("disabled", "");
   }
 });
 
+// User keyboard input
 window.addEventListener("keyup", e => {
   const keypress = e.key;
 
